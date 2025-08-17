d38b73db1a5a3c23407c313f27d64b8b42256b55
Connor <connorsmith8@gmail.com>
2025-08-17 09:02:34 -0500

    Initial Commit

---
+++ ParkIt/ParkItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkItApp.swift
+//  ParkIt
+//
+//  Created by Connor Smith on 8/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ParkItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParkItTests/ParkItTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParkItTests.swift
+//  ParkItTests
+//
+//  Created by Connor Smith on 8/17/25.
+//
+
+import Testing
+@testable import ParkIt
+
+struct ParkItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
